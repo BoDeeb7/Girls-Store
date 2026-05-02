69ec6683e5c23edffdcbff7e35a4453788c215f8
Hassan Deeb <hassandeeb473@gmail.com>
2026-05-02 23:45:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8871424613-592c8",
+  "appId": "1:466889827134:web:29348845e38310d9d2426f",
+  "apiKey": "AIzaSyChtTz1sywXIsC8y0TMwqWFPcbUsxTeulw",
+  "authDomain": "studio-8871424613-592c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "466889827134"
+};
